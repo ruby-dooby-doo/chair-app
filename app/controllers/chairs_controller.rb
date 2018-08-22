90f4c6053ec970239ea3fef6e90ef702ce836996
Brian <riceb53@gmail.com>
2018-08-21 20:57:07 -0500

    add new and create

--- app/controllers/chairs_controller.rb
+++ app/controllers/chairs_controller.rb
@@ -8,4 +8,19 @@ class ChairsController < ApplicationController
     @chair = Chair.find_by(id: params[:id])
     render 'show.html.erb'
   end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    chair = Chair.new(
+      color: params[:color],
+      material: params[:material],
+      height: params[:height],
+      legs: params[:legs]
+    )
+    chair.save
+    redirect_to "/chairs/#{chair.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   get '/chairs' => 'chairs#index'
+  get '/chairs/new' => 'chairs#new'
   get '/chairs/:id' => 'chairs#show'
+  post '/chairs' => 'chairs#create'
 end
